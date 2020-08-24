5ffb13d3071ae7b0a99cccca5c441d60e59db643
root <qionghuang6@gmail.com>
2020-08-24 18:26:30 +0000

    Added ability to get, add, delete entries using http requests with json

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const knex = require('knex')
-const pass = require('./pass') //pass.js is hidden from github!
+const express = require('express');
+const app = express();
+const port = 3000;
+const knex = require('knex');
+const pass = require('./pass'); //Low-security security :D
+const bp = require('body-parser');
+
+app.use(bp.json());
 
 const db = knex({                   
     client: 'pg',
@@ -14,11 +17,32 @@ const db = knex({
     }
   });
 
-db.select('*').from('entries').then(data => {
-    console.log(data);
+app.get('/', (req,res) => {
+  db.select('*').from('entries').then(data => {
+    res.json({entries: data});
+  });
+})
+
+app.post('/addentry', (req,res) => {
+  const {title, info, deadline, priority, star} = req.body; 
+  db('entries').insert({
+    title: title,
+    info: info,
+    deadline: deadline,
+    priority: priority,
+    star: star
+  })
+  .then(res.send('Added'))
+  .catch(err => {res.status(400).send('Error')})
 });
 
-
-app.listen(port, ()=> {
-    console.log('app is running on port ' + port);
+app.post('/delete', (req,res) => {
+  const {id} = req.body;
+  db('entries').where({id: id}).del()
+  .then(res.send('Deleted'))
+  .catch(err => {res.status(400).send('Error')})
 })
+
+app.listen(port, () => {
+  console.log('app is running on port ' + port);
+});
